98f6aa6dc0edbfadab45c6c280696d218e2af611
Soulaiman Bouhlal <bouhlal.soulaiman@gmail.com>
2024-10-12 19:08:29 +0100

    User managment

---
+++ src/main/java/com/ex/jakartalearn/entity/User.java
@@ -0,0 +1,48 @@
+package com.ex.jakartalearn.entity;
+
+import com.ex.jakartalearn.enumeration.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Task> tasks;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Request> requests;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Token token;
+
+}
---
+++ src/main/java/com/ex/jakartalearn/repository/UserRepository.java
@@ -0,0 +1,70 @@
+package com.ex.jakartalearn.repository;
+
+import com.ex.jakartalearn.entity.User;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Stateless
+public class UserRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public User save(User user) {
+        entityManager.persist(user);
+        return user;
+    }
+
+    public Optional<User> findByUsername(String username) {
+        try {
+            User user = entityManager.createQuery(
+                            "SELECT u FROM User u WHERE u.username = :username", User.class)
+                    .setParameter("username", username)
+                    .getSingleResult();
+            return Optional.of(user);
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<User> findByEmail(String email) {
+        try {
+            User user = entityManager.createQuery(
+                            "SELECT u FROM User u WHERE u.email = :email", User.class)
+                    .setParameter("email", email)
+                    .getSingleResult();
+            return Optional.of(user);
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<User> getById(Long id) {
+        User user =  entityManager.find(User.class, id);
+        return Optional.of(user);
+    }
+
+    @Transactional
+    public void update(User user) {
+        entityManager.merge(user);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Optional<User> user = getById(id);
+        if (user.isPresent()) {
+            entityManager.remove(user);
+        }
+    }
+
+    public List<User> getAll() {
+        return entityManager.createQuery("SELECT u FROM User u", User.class).getResultList();
+    }
+
+}
